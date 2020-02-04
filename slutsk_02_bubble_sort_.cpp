f22ad30a12ecab2217fe04a061a45c04f1cc07c5
slutsk <54749775+slutsk@users.noreply.github.com>
2020-02-04 19:17:27 +0300

    Add files via upload

---
+++ slutsk_02_bubble_sort_.cpp
@@ -0,0 +1,27 @@
+// Сортировка ПУЗЫРЕК (улучшенная)
+#include <iostream>
+using namespace std;
+int arr[100];
+void bubbleSort(int n){
+    for(int i = 0; i < n - 1; i++)
+        for(int j = 0; j < n - 1 - i; j++)
+            if(arr[j] > arr[j+1])
+                 swap(arr[j], arr[j+1]);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    for(int i=0; i<n; i++){
+        cout << arr[i] << ' ';
+    }
+    cout << endl;
+    bubbleSort(n);
+    for(int i=0; i<n; i++){
+        cout << arr[i] << ' ';
+    }
+
+}
