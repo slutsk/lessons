8f63aa31452f0812fbc26c752e27385b8f7de4b6
slutsk <54749775+slutsk@users.noreply.github.com>
2020-02-06 20:19:29 +0300

    Add files via upload

---
+++ slutsk_06_merge_sort.cpp
@@ -0,0 +1,43 @@
+// Merge Sort
+#include <iostream>
+using namespace std;
+void merge_sort(int A[], int a, int b){
+
+   if (a>=b) return;
+   int m=(a+b)/2;
+   merge_sort(A, a, m);
+   merge_sort(A, m+1, b);
+
+   int *tmp=new int[b-a+1];
+   int i = a;
+   int j = m+1;
+   for (int k = 0; k < b-a+1; k++)
+       if (i == m+1 || j != b+1 && A[j] < A[i]){
+           tmp[k] = A[j];
+           j++;
+       }
+       else{
+           tmp[k]=A[i];
+           i++;
+       }
+
+   for (int i = a, j = 0; i <= b; i++,j++)
+        A[i]=tmp[j];
+   delete tmp;
+}
+
+int main(){
+    int n;
+    int a[100];
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+
+    merge_sort(a, 0, n);
+
+    for(int i=0; i<n; i++){
+        cout << a[i] << ' ';
+    }
+
+}
