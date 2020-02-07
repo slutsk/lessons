992d55c1d49873a5adf24c350207f779a8a0a314
slutsk <54749775+slutsk@users.noreply.github.com>
2020-02-08 00:15:23 +0300

    Add files via upload

---
+++ slutsk_09_1_recursive_perebor.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void print_arr(){
+   for(int i = 0; i < n; ++i)
+      cout << arr[i];
+   cout << endl;
+}
+
+void rec(int  index, int &n, int &m){
+   if(index == n){
+      print_arr();
+      return;
+   }
+   for(int i = 1; i <= m; i++){
+      arr[index] = i;
+      rec(index + 1, n, m);
+   }
+}
+
+int main(){
+    int n, m;
+    cin >> n;
+    cin >> m;
+    int arr[100];
+    rec(0, n, m);
+    return 0;
+}
+
